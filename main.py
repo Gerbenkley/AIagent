0be6003e53a409969637659131b79249568b42ca
Gerbenkley <65512404+Gerbenkley@users.noreply.github.com>
2025-08-20 11:26:36 +0200

    added input, and verbose flag

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import os
+import sys
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
+
 def main():
+    #INITIALIZE API KEY
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    #CHECK IF GIVEN A PROMPT
+    if len(sys.argv) < 2:
+        print("Error: no input is given")
+        sys.exit(1)
+
+    #USER PROMPT
+    user_prompt = sys.argv[1]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+    
+    #CHECK THIRD ARGUMENT
+
+    #RESPONSE
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    #OUTPUT
+    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
     print("Response:")
     print(response.text)
 
